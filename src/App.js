da7aa312e81ac600af5a317f60000e707561320f
[Justin Cupp] <[jcupp35@gmail.com]>
2025-07-17 14:56:02 -0500

    used useState hook to manage the loggedIn state and defined login and logout functions to toggle the state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import HomePage from './HomePage';
+
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const login = () => setLoggedIn(true);
+  const logout = () => setLoggedIn(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loggedIn? (
+        <div>
+          <HomePage />
+          <LogoutButton logout={logout} />
+        </div>
+      ) : (
+          <LoginButton login={login} />
+      )}
+    </>
   );
 }
 
